124faf80ef194e6cf7ea7bfcacb0916b3c711af1
windowcow <strongwindowcow@gmail.com>
2024-03-16 19:42:08 +0900

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by windowcow on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
